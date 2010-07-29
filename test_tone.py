eb2972f04b3943df77527375b193caf4c0cd9c0c
Matteo Landi <landimatte@gmail.com>
2010-07-29 15:14:59 +0200

    Move GstToneGenerator inside the tone module.

--- test_tone.py
+++ test_tone.py
@@ -5,50 +5,7 @@ import gobject
 import gst
 import gtk
 
-from tone import ToneGenerator
-
-    
-class GstToneGenerator(ToneGenerator):
-    """Gstreamer based tone generator.
-    
-    Extend the base class ToneGenerator in order to control both the given
-    pipeline and source element with emitted signals.
-    """
-    
-    def __init__(self, pipeline, source):
-        """Constructor.
-        
-        Store the pipeline locally.
-        
-        Keywords:
-            pipeline gstreamer pipeline.
-            source element used to generate sounds.
-        """
-        super(GstToneGenerator, self).__init__()
-        self.connect('end-tone', self.end_tone_cb)
-        self.connect('start-tone', self.start_tone_cb)
-        self.connect('tone-value', self.tone_value_cb)
-        
-        self.pipeline = pipeline
-        self.source = source
-        
-    def end_tone_cb(self, widget):
-        """Stop the pipeline.
-        """
-        self.pipeline.set_state(gst.STATE_NULL)
-        
-    def start_tone_cb(self, widget):
-        """Start the pipeline.
-        
-        This method will make the pipeline to start reproducing sounds.
-        """
-        self.pipeline.set_state(gst.STATE_PLAYING)
-        
-    def tone_value_cb(self, widget, freq, volume):
-        """Change the values of frequency and volume with the given ones.
-        """
-        self.source.set_property('freq', freq)
-        self.source.set_property('volume', volume)
+from tone import GstToneGenerator
 
 
 if __name__ == '__main__':
--- tone.py
+++ tone.py
@@ -10,6 +10,7 @@ from math import pi
 
 import cairo
 import gobject
+import gst
 import gtk
 from gtk import gdk
 
@@ -36,7 +37,7 @@ class ToneGenerator(gtk.Window):
         """
         super(ToneGenerator, self).__init__()
 
-        self.freq = (100, 20000)
+        self.freq = (100, 10000)
         self.volume = (0.01, 1)
         
         self.connect('delete-event', self.delete_cb)
@@ -191,3 +192,45 @@ class ToneGenerator(gtk.Window):
         volume = max(v_min, min(10 ** v, v_max))
 
         return freq, volume
+    
+class GstToneGenerator(ToneGenerator):
+    """Gstreamer based tone generator.
+    
+    Extend the base class ToneGenerator in order to control both the given
+    pipeline and source element with emitted signals.
+    """
+    
+    def __init__(self, pipeline, source):
+        """Constructor.
+        
+        Store the pipeline locally.
+        
+        Keywords:
+            pipeline gstreamer pipeline.
+            source element used to generate sounds.
+        """
+        super(GstToneGenerator, self).__init__()
+        self.connect('end-tone', self.end_tone_cb)
+        self.connect('start-tone', self.start_tone_cb)
+        self.connect('tone-value', self.tone_value_cb)
+        
+        self.pipeline = pipeline
+        self.source = source
+        
+    def end_tone_cb(self, widget):
+        """Stop the pipeline.
+        """
+        self.pipeline.set_state(gst.STATE_NULL)
+        
+    def start_tone_cb(self, widget):
+        """Start the pipeline.
+        
+        This method will make the pipeline to start reproducing sounds.
+        """
+        self.pipeline.set_state(gst.STATE_PLAYING)
+        
+    def tone_value_cb(self, widget, freq, volume):
+        """Change the values of frequency and volume with the given ones.
+        """
+        self.source.set_property('freq', freq)
+        self.source.set_property('volume', volume)
